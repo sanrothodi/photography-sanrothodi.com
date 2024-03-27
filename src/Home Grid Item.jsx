e7b27a58319a824db0f2be3fc6fa2faaf9e228b6
Sanro <sanrothodi@protonmail.com>
2024-03-27 01:46:53 +0000

    Upload images

--- src/Home Grid Item.jsx
+++ src/Home Grid Item.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import sections from "./Sections";
+
+import PortraitImage from "../media/Home/Portrait.jpg";
+import LandscapeImage from "../media/Home/Landscape.jpg";
+
+// Automatically import all files ending in *.jpg from the media/Home folder
+// const imagesRequireContext = require.context("../media/Home", true, /\.jpg$/);
+
+// Map section names to imported images
+const imageMap = {
+  Portrait: PortraitImage,
+  Landscape: LandscapeImage,
+  // Add more mappings as necessary
+};
 
 const HomeGridContent = ({section}) => {
-    let link = "https://photography.sanrothodi.com/" + section;
-    let image = "media/home/" + section + ".jpg";
-    let item =  section + ' Photography';
+    let link = `https://photography.sanrothodi.com/${section}`;
+    let image = imageMap[section];
+    // let image = imagesRequireContext(`./${section}.jpg`).default;
+    let item =  `${section} Photography`;
     
     return(
             <div className="home-grid">
